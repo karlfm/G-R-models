f2ef8c3e6e4fac3eaaa67c8f0fc1f91b5a7d23a5
Karl Munthe <karlfredrik@Karl-sin-MacBook-Pro.local>
2024-03-01 10:42:56 +0100

    adding post processing module

---
+++ DDF/PostProcessing/postprocessing.py
@@ -0,0 +1,50 @@
+import dolfinx as df
+import ufl
+from dolfinx.io import XDMFFile
+from pathlib import Path
+
+# with XDMFFile(mesh.comm, "Hookean_facet_tags.xdmf", "w") as xdmf:
+#     xdmf.write_mesh(mesh)
+#     xdmf.write_meshtags(facet_tags, mesh.geometry)
+
+# "myHookeanDisplacement.xdmf"
+
+
+
+def write_to_paraview(filename, mesh, us):
+
+    # Not really sure about this
+    AA = ufl.VectorElement("Lagrange", mesh.ufl_cell(), 1)
+    BB = df.fem.FunctionSpace(mesh, AA)
+    u1 = df.fem.Function(BB)
+
+    filename = Path(filename)
+    filename.unlink(missing_ok=True)
+    filename.with_suffix(".h5").unlink(missing_ok=True)
+    fout = df.io.XDMFFile(mesh.comm, filename, "w")
+    fout.write_mesh(mesh)
+
+    for i, u in enumerate(us):
+        u1.interpolate(u)
+        fout.write_function(u1, i)
+
+def write_vm_to_paraview(filename, mesh, vms):
+
+    # Not really sure about this
+    # V_von_mises = df.fem.FunctionSpace(mesh, ("DG", 0))
+    # stresses = df.fem.Function(V_von_mises)
+
+    scalar_field = df.fem.functionspace(mesh, ("Lagrange", 1, (1, )))
+    stresses = df.fem.Function(scalar_field)
+
+    filename = Path(filename)
+    filename.unlink(missing_ok=True)
+    filename.with_suffix(".h5").unlink(missing_ok=True)
+    foutStress = df.io.XDMFFile(mesh.comm, filename, "w")
+    foutStress.write_mesh(mesh)
+
+    for i, vm in enumerate(vms):
+        print(i)
+        stress_expr = df.fem.Expression(vm, scalar_field.element.interpolation_points())
+        stresses.interpolate(stress_expr)
+        foutStress.write_function(stresses, i)
