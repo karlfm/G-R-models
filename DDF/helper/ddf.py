34f9c3c2ddec43cdccc21a2da2ee0d7d2605bb3b
Karl Munthe <karlfredrik@Karl-sin-MacBook-Pro.local>
2024-06-24 15:08:15 +0200

    backup for ddf

--- DDF/helper/ddf.py
+++ DDF/helper/ddf.py
@@ -1,25 +1,26 @@
 import numpy as np 
 import ufl
-import dolfinx as df
+import dolfinx
 import sys
 from mpi4py import MPI
 import basix
+from dataclasses import dataclass
 from dolfinx.io import XDMFFile
-
+from typing import Protocol
 
 sys.path.insert(1, '/MyProject/MyCode/DDF/Geometry')
-
 import geometry as geo
 
 def eval_expression(expr, mesh, point = [0.5, 0.5, 0.5]):
     # Determine what process owns a point and what cells it lies within
-    _, _, owning_points, cells = df.cpp.geometry.determine_point_ownership(
-        mesh._cpp_object, point, 1e-6)
+    arg1=np.array(point, dtype=np.float64)
+    _, _, owning_points, cells  = dolfinx.cpp.geometry.determine_point_ownership(
+        mesh._cpp_object, arg1 , 1e-6)
     owning_points = np.asarray(owning_points).reshape(-1, 3)
 
     # Pull owning points back to reference cell
     mesh_nodes = mesh.geometry.x
-    cmap = mesh.geometry.cmaps[0]
+    cmap = mesh.geometry.cmap
     ref_x = np.zeros((len(cells), mesh.geometry.dim),
                      dtype=mesh.geometry.x.dtype)
     for i, (point, cell) in enumerate(zip(owning_points, cells)):
@@ -27,18 +28,10 @@ def eval_expression(expr, mesh, point = [0.5, 0.5, 0.5]):
         ref_x[i] = cmap.pull_back(point.reshape(-1, 3), mesh_nodes[geom_dofs])
     if len(cells) > 0:
         # NOTE: Expression lives on only this communicator rank
-        d_expr = df.fem.Expression(expr, ref_x, comm=MPI.COMM_SELF)
+        d_expr = dolfinx.fem.Expression(expr, ref_x, comm=MPI.COMM_SELF)
         values = d_expr.eval(mesh, np.asarray(cells).astype(np.int32))
         return values
-
-def plot_boundaries(mesh, facet_tags):
-    mesh.topology.create_connectivity(
-        mesh.topology.dim-1, mesh.topology.dim
-        )
-    with XDMFFile(mesh.comm, "CheckBoundaries.xdmf", "w") as xdmf:
-        xdmf.write_mesh(mesh)
-        xdmf.write_meshtags(facet_tags, mesh.geometry)
-        
+    
 def set_boundary_types(mesh, boundary_type, X):
     # boundary_conditions is a list of pairs of ints containing boundary conditions for each dimension
     boundary_conditions_dolfin = []
@@ -57,7 +50,7 @@ def set_boundary_types(mesh, boundary_type, X):
     flattened_boundaries = np.hstack(flattened_boundaries)
     boundary_conditions_dolfin = np.hstack(boundary_conditions_dolfin)
     sort_array = np.argsort(flattened_boundaries)
-    facet_tags = df.mesh.meshtags(
+    facet_tags = dolfinx.mesh.meshtags(
         mesh, boundary_dimension, flattened_boundaries[sort_array], boundary_conditions_dolfin[sort_array]
     )
 
@@ -65,7 +58,6 @@ def set_boundary_types(mesh, boundary_type, X):
     return facet_tags
 
 def dirichlet_injection(mesh, bc_values, function_space):
-
     all_facet_tags = {}
     boundary_conditions = []
     facet_dimension = mesh.topology.dim - 1
@@ -73,71 +65,142 @@ def dirichlet_injection(mesh, bc_values, function_space):
     points = []
     values = []
     for i, (bc_locator, component, value) in enumerate(bc_values, start=1):
-        boundary_points = df.mesh.locate_entities_boundary(mesh, facet_dimension, bc_locator)
+        boundary_points = dolfinx.mesh.locate_entities_boundary(mesh, facet_dimension, bc_locator)
         points.append(boundary_points)
-        values.append(np.ones(len(boundary_points))*i)
-        boundary_dofs = df.fem.locate_dofs_topological(function_space.sub(component), facet_dimension, boundary_points)
-        boundary_condition = df.fem.dirichletbc(value, boundary_dofs, function_space.sub(component))
+        values.append(np.ones(len(boundary_points))*i)  # This is just for the facet tags; not the boundary condition
+        boundary_dofs = dolfinx.fem.locate_dofs_topological(function_space.sub(component), facet_dimension, boundary_points)
+        boundary_condition = dolfinx.fem.dirichletbc(value, boundary_dofs, function_space.sub(component))
         boundary_conditions.append(boundary_condition)
 
-    facet_tags = df.mesh.meshtags(
+    facet_tags = dolfinx.mesh.meshtags(
         mesh, facet_dimension, np.hstack(points), np.hstack(values).astype(np.int32)
         )
-
 
     all_facet_tags[f"{component}_{value}"] = facet_tags
 
     return boundary_conditions, all_facet_tags
 
+def dirichlet_injection_ellipsoid(geometry, bc_values, function_space):
+
+    boundary_conditions = []
+    facet_dimension = geometry.mesh.topology.dim - 1
+
+
+    for bc_location, component, value in bc_values:
+        boundary_points = geometry.ffun.find(geometry.markers[bc_location][0])  # Specify the marker used on the boundary
+        geometry.mesh.topology.create_connectivity(
+            geometry.mesh.topology.dim - 1,
+            geometry.mesh.topology.dim,
+        )
+        boundary_dofs = dolfinx.fem.locate_dofs_topological(function_space.sub(component), facet_dimension, boundary_points)
+        boundary_condition = dolfinx.fem.dirichletbc(value, boundary_dofs, function_space.sub(component))
+        boundary_conditions.append(boundary_condition)
+
+    return boundary_conditions
+
+def neumann_injection_ellipsoid(geometry, bc_info, P, F, v):
+
+    n = ufl.FacetNormal(geometry.mesh)
+
+    for bc_location, value in bc_info:   # TODO: implement shear Neumann BC
+        # n = value * ufl.cofac(F) * N
+        P += ufl.inner(v, value * ufl.cofac(F) * n) * ufl.ds(subdomain_id=geometry.markers[bc_location][0], subdomain_data=geometry.ffun)
+
+    return P
+
 def neumann_injection(mesh, bc_info, P, v):
     n = ufl.FacetNormal(mesh)
-    x = ufl.SpatialCoordinate(mesh)
     
     facet_indices, facet_markers = [], []
-    fdim = mesh.topology.dim - 1
+    facet_dimension = mesh.topology.dim - 1
     for marker, locator, _ in bc_info:
-        facets = df.mesh.locate_entities(mesh, fdim, locator)
+        facets = dolfinx.mesh.locate_entities(mesh, facet_dimension, locator)
         facet_indices.append(facets)
         facet_markers.append(np.full_like(facets, marker))
     facet_indices = np.hstack(facet_indices).astype(np.int32)
     facet_markers = np.hstack(facet_markers).astype(np.int32)
     sorted_facets = np.argsort(facet_indices)
-    facet_tag = df.mesh.meshtags(mesh, fdim, facet_indices[sorted_facets], facet_markers[sorted_facets])
+    facet_tag = dolfinx.mesh.meshtags(mesh, facet_dimension, facet_indices[sorted_facets], facet_markers[sorted_facets])
 
-    for _, _, values in bc_info:  
+    for marker, _, values in bc_info:  
         P -= ufl.inner(values * v, n) * ufl.ds(subdomain_id=marker, subdomain_data=facet_tag)
 
     return P
 
+
+def robin_injection(mesh, bc_info, P, v):
+    n = ufl.FacetNormal(mesh)
+    
+    facet_indices, facet_markers = [], []
+    facet_dimension = mesh.topology.dim - 1
+    for marker, locator, _ in bc_info:
+        facets = dolfinx.mesh.locate_entities(mesh, facet_dimension, locator)
+        facet_indices.append(facets)
+        facet_markers.append(np.full_like(facets, marker))
+    facet_indices = np.hstack(facet_indices).astype(np.int32)
+    facet_markers = np.hstack(facet_markers).astype(np.int32)
+    sorted_facets = np.argsort(facet_indices)
+    facet_tag = dolfinx.mesh.meshtags(mesh, facet_dimension, facet_indices[sorted_facets], facet_markers[sorted_facets])
+
+    for marker, _, values in bc_info:  
+        P -= ufl.inner(values * v, n) * ufl.ds(subdomain_id=marker, subdomain_data=facet_tag)
+
+    return P
+
+
+
+def get_facet_tags(mesh, marker, locator):
+        
+    facet_indices, facet_markers = [], []
+    facet_dimension = mesh.topology.dim - 1
+    facets = dolfinx.mesh.locate_entities(mesh, facet_dimension, locator)
+    facet_indices.append(facets)
+    facet_markers.append(np.full_like(facets, marker))
+    facet_indices = np.hstack(facet_indices).astype(np.int32)
+    facet_markers = np.hstack(facet_markers).astype(np.int32)
+    sorted_facets = np.argsort(facet_indices)
+    facet_tag = dolfinx.mesh.meshtags(mesh, facet_dimension, facet_indices[sorted_facets], facet_markers[sorted_facets])
+
+    return facet_tag
+
+
+
+
 def get_functions(mesh):
-    vector_element = ufl.VectorElement(family="Lagrange", cell=mesh.ufl_cell(), degree=2) #, dim=mesh.geometry.dim
-    V = df.fem.FunctionSpace(mesh, vector_element)
-    u = df.fem.Function(V)         # solution
+    # vector_element = ufl.vectorElement(family="Lagrange", cell=mesh.ufl_cell(), degree=1) #, dim=mesh.geometry.dim
+    vector_element = basix.ufl.element(family="Lagrange", cell=str(mesh.ufl_cell()), degree=1, shape=(mesh.geometry.dim, ))
+    V = dolfinx.fem.functionspace(mesh, vector_element)
+    u = dolfinx.fem.Function(V)         # solution
     v = ufl.TestFunction(V)        # test function
 
     return V, u, v
 
 def to_scalar_map(f, mesh):
     # You have to interpolate the function like function.interpolate(expression)
-    scalar_field = df.fem.FunctionSpace(mesh, ("Lagrange", 1, (1, )))
-    expression = df.fem.Expression(f, scalar_field.element.interpolation_points())
-    function = df.fem.Function(scalar_field)
+    scalar_field = dolfinx.fem.functionspace(mesh, ("Lagrange", 1, (1, )))
+    expression = dolfinx.fem.Expression(f, scalar_field.element.interpolation_points())
+    function = dolfinx.fem.Function(scalar_field)
     return function, expression
 
-def to_tensor_map(f, mesh):
+def to_tensor_map(f, mesh, shape = (3,3)):
     # You have to interpolate the function like function.interpolate(expression)
-    tensor_field = df.fem.FunctionSpace(mesh, ufl.TensorElement("DG", mesh.ufl_cell(), 0, shape=(3,3)))
-    expression = df.fem.Expression(f, tensor_field.element.interpolation_points())
-    function = df.fem.Function(tensor_field)
+    if shape is (1,1):
+        tensor_field = dolfinx.fem.functionspace(mesh, "DG", 0)
+    else:
+        tensor_field = dolfinx.fem.functionspace(mesh, basix.ufl.element(family="DG", cell=str(mesh.ufl_cell()), degree=0, shape=shape))
+
+    expression = dolfinx.fem.Expression(f, tensor_field.element.interpolation_points())
+    function = dolfinx.fem.Function(tensor_field)
+    
     return function, expression
 
 def interpolate_quadrature(ufl_expr, mesh):
 
     We = ufl.FiniteElement("Quadrature", mesh.ufl_cell(), degree = mesh.geometry.dim, quad_scheme = "default")
 
-    W  = df.fem.FunctionSpace(mesh, We)
+    W  = dolfinx.fem.functionspace(mesh, We)
 
-    new_stress = df.fem.Function(W)
+    new_stress = dolfinx.fem.Function(W)
 
     basix_celltype = getattr(basix.CellType, mesh.topology.cell_types[0].name)
     quadrature_points, weights = basix.make_quadrature(basix_celltype,1)
@@ -146,7 +209,7 @@ def interpolate_quadrature(ufl_expr, mesh):
     num_cells = map_c.size_local + map_c.num_ghosts
     cells = np.arange(0, num_cells, dtype=np.int32)
 
-    expr_expr = df.fem.Expression(ufl_expr, quadrature_points)
+    expr_expr = dolfinx.fem.Expression(ufl_expr, quadrature_points)
     expr_eval = expr_expr.eval(mesh, cells)
     return np.mean(expr_eval, axis=0)
 
@@ -195,7 +258,7 @@ def solver(F, u, bcs):
     # F is deformation tensor
     # u is solution
     # bcs is a list of boundary condiiotns
-    problem = df.fem.petsc.NonlinearProblem(F, u, bcs)
+    problem = dolfinx.fem.petsc.NonlinearProblem(F, u, bcs)
     return problem
 
 
@@ -205,5 +268,5 @@ def main():
 if __name__ == "__main__":
     main()
 
-# problem = df.fem.petsc.NonlinearProblem(weak_form, u, boundaryConditions)
-# solver = df.nls.petsc.NewtonSolver(mesh.comm, problem)
+# problem = dolfinx.fem.petsc.NonlinearProblem(weak_form, u, boundaryConditions)
+# solver = dolfinx.nls.petsc.NewtonSolver(mesh.comm, problem)
